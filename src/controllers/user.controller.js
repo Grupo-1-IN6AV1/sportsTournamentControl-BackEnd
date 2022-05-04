a23dfc6227b6e1ad416d8ab8f90bc903e4c40873
sescobar-2020031 <sescobar-2020031@kinal.edu.gt>
2022-05-03 22:06:36 -0600

    Se agregó la función login en el archivo -user.controller.js-

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -37,3 +37,26 @@ exports.register = async(req, res)=>{
         return res.status(500).send({err, message: 'Error saving user'});
     }
 }
+
+exports.login = async(req, res)=>{
+    try{
+        const params = req.body;
+        let data = {
+            username: params.username,
+            password: params.password
+        }
+        let msg = validateData(data);
+
+        if(msg) return res.status(400).send(msg);
+        let already = await alreadyUser(params.username);
+        if(already && await checkPassword(data.password, already.password)){
+            let token = await jwt.createToken(already);
+            delete already.password;
+
+            return res.send({token, message: 'Login successfuly', already});
+        }else return res.status(401).send({message: 'Invalid credentials'});
+    }catch(err){
+        console.log(err);
+        return res.status(500).send({err, message: 'Failed to login'});
+    }
+}
