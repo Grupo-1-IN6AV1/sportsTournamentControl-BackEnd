69f95070fe374f65affddb4db53a4f1130a6a5ac
mlopez-2020202 <mlopez-2020202@kinal.edu.gt>
2022-05-03 21:45:02 -0600

    Se agregó el archivo -user.controller.js- y las funciones registrar y test.

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const User = require('../models/user.model');
+const {validateData, encrypt, alreadyUser, 
+       } = require('../utils/validate');
+
+//FUNCIONES PÚBLICAS
+
+exports.userTest = async (req, res)=>{
+    await res.send({message: 'Controller run'})
+}
+
+exports.register = async(req, res)=>{
+    try{
+        const params = req.body;
+        let data = {
+            name: params.name,
+            username: params.username,
+            email: params.email,
+            password: params.password,
+            role: 'CLIENT'
+        };
+        let msg = validateData(data);
+
+        if(msg) return res.status(400).send(msg);
+        let already = await alreadyUser(data.username);
+        if(already) return res.status(400).send({message: 'Username already in use'});
+        data.surname = params.surname;
+        data.phone = params.phone;
+        data.password = await encrypt(params.password);
+
+        let user = new User(data);
+        await user.save();
+        return res.send({message: 'User created successfully'});
+    }catch(err){
+        console.log(err);
+        return res.status(500).send({err, message: 'Error saving user'});
+    }
+}
