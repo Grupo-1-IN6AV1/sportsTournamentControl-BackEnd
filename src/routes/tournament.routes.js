389e8ac0e272dbff1fa1d5c51593dcf2d5470440
mlopez-2020202 <mlopez-2020202@kinal.edu.gt>
2022-05-06 22:37:58 -0600

    Se completaron las routes de -Tournament-

--- src/routes/tournament.routes.js
+++ src/routes/tournament.routes.js
@@ -9,5 +9,11 @@ api.get('/tournamentTest', tournamentController.tournamentTest);
 api.post('/createTournament', mdAuth.ensureAuth, tournamentController.createTournament);
 api.get('/getTournaments', mdAuth.ensureAuth, tournamentController.viewTournaments);
 api.get('/getTournament/:id', mdAuth.ensureAuth, tournamentController.viewTournament);
+api.put('/updateTournament/:id', mdAuth.ensureAuth, tournamentController.updateTournament);
+api.put('/updateTournamentByAdmin/:id', [mdAuth.ensureAuth, mdAuth.isAdmin], tournamentController.updateTournamentByAdmin);
+api.delete('/deleteTournament/:id', mdAuth.ensureAuth, tournamentController.deleteTournament);
+api.delete('/deleteTournamentByAdmin/:id', [mdAuth.ensureAuth, mdAuth.isAdmin], tournamentController.deleteTournamentByAdmin);
+api.post('/addTeamtoTournament/:id', mdAuth.ensureAuth, tournamentController.addTeamIntoTournamnet);
+api.put('/removeTeamToTournament/:id', mdAuth.ensureAuth, tournamentController.removeTeamToTournament);
 
 module.exports = api;
