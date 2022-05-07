665f9c89d9e02df6f61e8dd6f0fdbe71a4fe7802
mlopez-2020202 <mlopez-2020202@kinal.edu.gt>
2022-05-06 22:32:56 -0600

    Se actualizó el modelo de -Tournament-

--- src/models/tournament.model.js
+++ src/models/tournament.model.js
@@ -5,7 +5,11 @@ const mongoose = require('mongoose');
 const tournamentSchema = mongoose.Schema({
     name: String,
     description: String,
-    user: {type: mongoose.Schema.ObjectId, ref: 'User'}
+    user: {type: mongoose.Schema.ObjectId, ref: 'user'},
+    journey: {type: mongoose.Schema.ObjectId, ref: 'journey'},
+    teams: [
+        {type: mongoose.Schema.ObjectId, ref: 'team'}
+    ] 
 });
 
 module.exports = mongoose.model('Tournament', tournamentSchema);
