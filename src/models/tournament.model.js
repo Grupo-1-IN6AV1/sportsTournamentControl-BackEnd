9d557be3c591128de23635828b941b2340f397a2
rcardenas-2020166 <rcardenas-2020166@kinal.edu.gt>
2022-05-08 12:42:11 -0600

    Se modificó la estructura y funcionamiento de -Teams-.

--- src/models/tournament.model.js
+++ src/models/tournament.model.js
@@ -6,8 +6,8 @@ const tournamentSchema = mongoose.Schema({
     name: String,
     description: String,
     user: {type: mongoose.Schema.ObjectId, ref: 'User'},
-    journeys: [{journey:{type: mongoose.Schema.ObjectId, ref: 'Journey'}}],
-    teams: [ {team:{type: mongoose.Schema.ObjectId, ref: 'Team'}}] 
+    journeys: [{type: mongoose.Schema.ObjectId, ref: 'Journey'}],
+    teams: [ {type: mongoose.Schema.ObjectId, ref: 'Team'}] 
 });
 
 module.exports = mongoose.model('Tournament', tournamentSchema);
--- src/routes/team.routes.js
+++ src/routes/team.routes.js
@@ -5,14 +5,18 @@ const mdAuth = require('../services/authenticated');
 const express = require('express');
 const api = express.Router();
 
-
-api.get('/teamTest', teamController.testTeam);
+//RUTAS DEL USUARIO//
 api.post('/createTeam', mdAuth.ensureAuth, teamController.createTeam);
-api.put('/updateTeam/:id', mdAuth.ensureAuth, teamController.updateTeam);
 api.get('/getTeamUser/:id', mdAuth.ensureAuth, teamController.getTeamUser);
 api.get('/getTeamsUser', mdAuth.ensureAuth, teamController.getTeamsUser);
-api.post('/searchTeamsUser', mdAuth.ensureAuth, teamController.searchTeamsUser);
 api.delete('/deleteTeam/:id', mdAuth.ensureAuth, teamController.deleteTeam);
+api.put('/updateTeam/:id', mdAuth.ensureAuth, teamController.updateTeam);
+
+//RUTAS DEL ADMINISTRADOR//
+api.get('/getTeamAdmin/:id', [mdAuth.ensureAuth, mdAuth.isAdmin], teamController.getTeamAdmin);
+api.get('/getTeamsAdmin', [mdAuth.ensureAuth, mdAuth.isAdmin], teamController.getTeamsAdmin);
+api.put('/updateTeamAdmin/:id', [mdAuth.ensureAuth, mdAuth.isAdmin], teamController.updateTeamAdmin);
+api.delete('/deleteTeamAdmin/:id', [mdAuth.ensureAuth, mdAuth.isAdmin], teamController.deleteTeamAdmin);
 
 
 module.exports = api;
